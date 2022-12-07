9fff4b20ca94d5db0f5d5017e77e5e04e1ec0ffd
Varghese <varghesereji0007@gmail.com>
2022-12-07 09:44:42 +0530

    App problem 1

--- APP/Ass2/Problem_1.py
+++ APP/Ass2/Problem_1.py
@@ -3,6 +3,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+
+ALPHA_UPPER = 0.9
+ALPHA_CENTRAL = 0.68
 
 
 def factorial(n):
@@ -22,6 +26,16 @@ def poisson_function(x_array, mu):
     for n in x_array:
         func_values.append(poisson_distribution(n, mu))
     return np.array(func_values)
+
+
+def gaussian_function(x_array, mu):
+    return (1/(np.sqrt(2*np.pi))) * np.exp(-(x_array-mu)**2/2)
+
+
+def integral_gaussian(x_0, x_u, mu):
+    print(x_0, x_u)
+    Integral = integrate.quad(gaussian_function, x_0, x_u, args=mu)
+    return Integral[0]
 
 
 def summation(array):
@@ -57,66 +71,134 @@ def central_upper_limit(cl, alpha):
         return False
 
 
+def poisson():
+    x = np.arange(0, 10, 1)
+    mu = 0
+    # Upper Limit
+    x_vs = []
+    mu_vs = []
+    while mu < 10:
+        pdf = poisson_function(x, mu)
+        for q, P in enumerate(pdf):
+            cl = summation(pdf[:q+1])
+            print(x[q], P, cl)
+            if upper_limit(cl, ALPHA_UPPER) is True:
+                print('we got it')
+                x_vs.append(x[q])
+                mu_vs.append(mu)
+                break
+        mu += 1
 
-x = np.arange(0, 10, 1)
+    plt.figure()
+    plt.plot(x_vs, mu_vs, 'o-')
+    plt.title('Poisson, Upper limit 90%')
+    plt.xlabel('Measured x')
+    plt.ylabel('$\mu$')
+    plt.grid()
+    # plt.show(block=False)
+    plt.savefig('poisson_upper.png')
 
-alpha_upper = 0.9
-mu = 0
+    # Central Interval
+    print('Central Interval')
+    x_1 = []
+    x_2 = []
+    mu_vs_cl = []
+    mu = 0
+    while mu < 10:
+        pdf = poisson_function(x, mu)
+        for q, P in enumerate(pdf):
+            cl = summation(pdf[:q+1])
+            print(x[q], P, cl)
+            if central_lower_limit(cl, ALPHA_CENTRAL) is True:
+                print('we got lower it')
 
-# Upper Limit
-x_vs = []
-mu_vs = []
-while mu < 10:
-    pdf = poisson_function(x, mu)
-    for q, P in enumerate(pdf):
-        cl = summation(pdf[:q+1])
-        print(x[q], P, cl)
-        if upper_limit(cl, alpha_upper) is True:
-            print('we got it')
-            x_vs.append(x[q])
-            mu_vs.append(mu)
-            break
-    mu += 1
+                for r, l in enumerate(x[q+1:]):
+                    print(r, q, r+q+1)
+                    cl_u = summation(pdf[-(r+1):-1])
+                    if central_upper_limit(cl_u, ALPHA_CENTRAL) is True:
+                        x_2.append(x[-r-1])
+                        x_1.append(x[q])
+                        mu_vs_cl.append(mu)
+                        break
+                break
+        mu += 1
+    print(x_1)
+    print(x_2)
+    plt.figure()
+    plt.plot(x_1, mu_vs_cl, 'o-')
+    plt.plot(x_2, mu_vs_cl, 'o-')
+    plt.title('Poisson, central limit 68%')
+    plt.xlabel('Measured x')
+    plt.ylabel('$\mu$')
+    plt.grid()
+    # plt.show(block=False)
+    plt.savefig('poisson_central.png')
 
-plt.figure()
-plt.plot(x_vs, mu_vs, 'o-')
-plt.title('Poisson, Upper limit 90%')
-plt.xlabel('Measured x')
-# plt.show(block=False)
-plt.savefig('poisson_upper.png')
 
-# Central Interval
-print('Central Interval')
-alpha_central = 0.68
-x_1 = []
-x_2 = []
-mu_vs_cl = []
-mu = 0
-while mu < 10:
-    pdf = poisson_function(x, mu)
-    for q, P in enumerate(pdf):
-        cl = summation(pdf[:q+1])
-        print(x[q], P, cl)
-        if central_lower_limit(cl, alpha_central) is True:
-            print('we got lower it')
+def gaussian():
+    x = np.arange(0.1, 10, 0.1)
+    mu = 0
+    # Upper Limit
+    x_vs = []
+    mu_vs = []
+    while mu < 10:
+        pdf = gaussian_function(x, mu)
+        for q, P in enumerate(pdf):
+            cl = integral_gaussian(0, x[q], mu)
+            print(x[q], P, cl)
+            if upper_limit(cl, ALPHA_UPPER) is True:
+                print('we got it')
+                x_vs.append(x[q])
+                mu_vs.append(mu)
+                break
+        mu += 1
 
-            for r, l in enumerate(x[q+1:]):
-                print(r, q, r+q+1)
-                r += q+1
-                cl_u = summation(pdf[r:])
-                if central_upper_limit(cl_u, alpha_central) is True:
-                    x_2.append(x[r])
-                    x_1.append(x[q])
-                    mu_vs_cl.append(mu)
-                    break
-            break
-    mu += 1
-print(x_1)
-print(x_2)
-plt.figure()
-plt.plot(x_1, mu_vs_cl, 'o-')
-plt.plot(x_2, mu_vs_cl, 'o-')
-plt.title('Poisson, central limit 68%')
-plt.xlabel('Measured x')
-# plt.show(block=False)
-plt.savefig('poisson_central.png')
+    plt.figure()
+    plt.plot(x_vs, mu_vs, 'o-')
+    plt.title('Gaussian, Upper limit 90%')
+    plt.xlabel('Measured x')
+    plt.ylabel('$\mu$')
+    plt.grid()
+    # plt.show(block=False)
+    plt.savefig('gaussian_upper.png')
+
+    # Central Interval
+    print('Central Interval')
+    x_1 = []
+    x_2 = []
+    mu_vs_cl = []
+    mu = 0
+    while mu < 10:
+        pdf = gaussian_function(x, mu)
+        for q, P in enumerate(pdf):
+            cl = integral_gaussian(0, x[q], mu)
+            print(x[q], P, cl)
+            if central_lower_limit(cl, ALPHA_CENTRAL) is True:
+                print('we got lower it')
+
+                for r, l in enumerate(x[q+1:]):
+                    print(r, q)
+                    cl_u = integral_gaussian(x[-r-1], x[-1], mu)
+                    if central_upper_limit(cl_u, ALPHA_CENTRAL) is True:
+                        x_2.append(x[-r-1])
+                        x_1.append(x[q])
+                        mu_vs_cl.append(mu)
+                        break
+                break
+        mu += 1
+    print(x_1)
+    print(x_2)
+    plt.figure()
+    plt.plot(x_1, mu_vs_cl, 'o-')
+    plt.plot(x_2, mu_vs_cl, 'o-')
+    plt.title('Gaussian, central limit 68%')
+    plt.xlabel('Measured x')
+    plt.ylabel('$\mu$')
+    plt.grid()
+    # plt.show(block=False)
+    plt.savefig('gaussian_central.png')
+
+
+gaussian()
+poisson()
+# Code ends here
